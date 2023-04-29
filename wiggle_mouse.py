387d047654372f9689fc39610cc6cb4efd280809
Trevor French <42419234+TrevorFrench@users.noreply.github.com>
2023-04-29 14:15:16 -0600

    Add files via upload

---
+++ wiggle_mouse.py
@@ -0,0 +1,18 @@
+
+import pyautogui
+import datetime
+import time
+
+now = datetime.datetime.now()
+current_time = now.time()
+begin = datetime.time(9, 0, 0)
+end = datetime.time(17, 0, 0)
+
+while current_time > begin and current_time < end:
+    now = datetime.datetime.now()
+    current_time = now.time()
+    pyautogui.moveRel(0, 75, duration = 1)
+    pyautogui.moveRel(-75, 0, duration = 1)
+    pyautogui.moveRel(0, -75, duration = 1)
+    pyautogui.moveRel(75, 0, duration = 1)
+    time.sleep(5)
