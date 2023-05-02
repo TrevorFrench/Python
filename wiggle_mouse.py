f4cfd1783ff5652c45860e3748697ae7d2871d30
Trevor French <42419234+TrevorFrench@users.noreply.github.com>
2023-05-01 20:01:27 -0600

    Update wiggle_mouse.py

--- wiggle_mouse.py
+++ wiggle_mouse.py
@@ -1,13 +1,13 @@
-
+# MODULES
 import pyautogui
 import datetime
 import time
-
+# TIME PARAMETERS
 now = datetime.datetime.now()
 current_time = now.time()
 begin = datetime.time(9, 0, 0)
 end = datetime.time(17, 0, 0)
-
+# WIGGLE LOOP
 while current_time > begin and current_time < end:
     now = datetime.datetime.now()
     current_time = now.time()
